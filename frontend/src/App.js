eaa5be4926eb23a78cc30304e3af0b2eca70b873
Harsh Patel <48337893+harshpatel010202@users.noreply.github.com>
2023-07-08 12:52:33 -0700

    make fake data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import ClubPage from "./Components/ClubPage";
+import Event from "./Components/club_more_info/Event";
 
 function App() {
   return (
     <div className="App">
-      <ClubPage />
+      <Event />
     </div>
   );
 }
--- frontend/src/Events/Event.js
+++ frontend/src/Events/Event.js
@@ -10,4 +10,69 @@ const Event = ({ name, description, image }) => {
   );
 };
 
-export default Event;
+export { Event };
+
+// Generating fake data
+const fakeEvents = [
+  {
+    name: "UVic Programming Club",
+    event: "Competitive Programming Contest",
+    description: "TODO: Add description here",
+    image: "https://example.com/fake-image.jpg",
+  },
+  {
+    name: "UVic	CyberSec Club",
+    event: "Hackathon",
+    description: "TODO: Add description here",
+    image: "https://example.com/another-fake-image.jpg",
+  },
+
+  {
+    name: "UVic Volleyball",
+    event: "Volleyball Intramurals",
+    description: "TODO: Add description here",
+    image: "https://example.com/another-fake-image.jpg",
+  },
+
+  {
+    name: "UVic Badminton",
+    event: "Badminton Intramurals",
+    description: "TODO: Add description here",
+    image: "https://example.com/another-fake-image.jpg",
+  }
+  // Add more fake events here...
+  // {
+  //   name: "Event 3",
+  //   description: "Description for Event 3",
+  //   image: "https://example.com/event3-image.jpg",
+  //   // category: "Category"
+  // },
+  // {
+  //   name: "Event 4",
+  //   description: "Description for Event 4",
+  //   image: "https://example.com/event4-image.jpg",
+  //   // category: "Category"
+  // },
+  // ...
+];
+
+// Usage in your component
+function App() {
+  return (
+    <div>
+      {fakeEvents.map((event, index) => (
+        <Event
+          key={index}
+          name={event.name}
+          description={event.description}
+          image={event.image}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default App;
+
+
+//fake data taken from https://uvss.ca/wp-content/uploads/2020/06/CURRENT-CLUBS.pdf, https://vikesrec.ca/intramurals/volleyball
