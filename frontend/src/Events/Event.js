60c7d8e21045f7d5fbb62671a13ec128241dc145
Harsh Patel <48337893+harshpatel010202@users.noreply.github.com>
2023-07-08 13:02:51 -0700

    fake events

--- frontend/src/Events/Event.js
+++ frontend/src/Events/Event.js
@@ -17,43 +17,45 @@ const fakeEvents = [
   {
     name: "UVic Programming Club",
     event: "Competitive Programming Contest",
-    description: "TODO: Add description here",
+    description: "Join the UVic Programming Club for an exciting Competitive Programming Contest! Test your coding skills, solve challenging problems, and compete with fellow programmers. Open to all skill levels, this event will provide a great opportunity to enhance your programming abilities and connect with like-minded individuals.",
     image: "https://example.com/fake-image.jpg",
   },
   {
-    name: "UVic	CyberSec Club",
+    name: "UVic CyberSec Club",
     event: "Hackathon",
-    description: "TODO: Add description here",
+    description: "Participate in the UVic CyberSec Club's Hackathon and dive into the world of cybersecurity. Collaborate with fellow hackers to solve real-world challenges, explore vulnerabilities, and develop innovative solutions. Whether you are a beginner or an experienced hacker, this event will offer a platform to learn, create, and network with cybersecurity enthusiasts.",
     image: "https://example.com/another-fake-image.jpg",
   },
-
   {
     name: "UVic Volleyball",
     event: "Volleyball Intramurals",
-    description: "TODO: Add description here",
+    description: "Get ready to bump, set, and spike at the UVic Volleyball Intramurals! Gather your friends, form a team, and showcase your volleyball skills in a friendly and competitive environment. Whether you are a seasoned player or a beginner, this event welcomes players of all levels. Join us for an exciting and fun-filled day of volleyball action!",
     image: "https://example.com/another-fake-image.jpg",
   },
-
   {
     name: "UVic Badminton",
     event: "Badminton Intramurals",
-    description: "TODO: Add description here",
+    description: "Calling all badminton enthusiasts! Join the UVic Badminton Intramurals and engage in thrilling matches on the court. Whether you are a singles expert or a doubles specialist, this event offers a fantastic opportunity to showcase your skills and compete against fellow badminton players. Grab your racket and get ready for an exciting badminton showdown!",
     image: "https://example.com/another-fake-image.jpg",
+  },
+  {
+    name: "UVic Photography Club",
+    event: "Photography Workshop",
+    description: "Join the UVic Photography Club for a hands-on Photography Workshop. Learn essential techniques, composition tips, and post-processing tricks to enhance your photography skills. Whether you are a beginner or an experienced photographer, this workshop will provide valuable insights and an opportunity to capture stunning images in a supportive environment.",
+    image: "https://example.com/photography-workshop.jpg",
+  },
+  {
+    name: "UVic Entrepreneurship Society",
+    event: "Startup Pitch Competition",
+    description: "Calling all aspiring entrepreneurs! Participate in the UVic Entrepreneurship Society's Startup Pitch Competition. Showcase your innovative business idea to a panel of judges and compete for valuable prizes and networking opportunities. Whether you have a well-developed business plan or just a brilliant concept, this competition will provide a platform to pitch your ideas and gain valuable feedback.",
+    image: "https://example.com/startup-pitch.jpg",
+  },
+  {
+    name: "UVic Environmental Society",
+    event: "Beach Cleanup",
+    description: "Make a positive impact on the environment by participating in the UVic Environmental Society's Beach Cleanup. Join a group of passionate volunteers and help remove litter and debris from a local beach. Together, we can contribute to the preservation of our coastal ecosystems and raise awareness about the importance of environmental conservation.",
+    image: "https://example.com/beach-cleanup.jpg",
   }
-  // Add more fake events here...
-  // {
-  //   name: "Event 3",
-  //   description: "Description for Event 3",
-  //   image: "https://example.com/event3-image.jpg",
-  //   // category: "Category"
-  // },
-  // {
-  //   name: "Event 4",
-  //   description: "Description for Event 4",
-  //   image: "https://example.com/event4-image.jpg",
-  //   // category: "Category"
-  // },
-  // ...
 ];
 
 // Usage in your component
@@ -73,6 +75,3 @@ function App() {
 }
 
 export default App;
-
-
-//fake data taken from https://uvss.ca/wp-content/uploads/2020/06/CURRENT-CLUBS.pdf, https://vikesrec.ca/intramurals/volleyball
