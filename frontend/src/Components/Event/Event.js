db952f1e269506ea9f9c673143fbc561b5bf33e7
kevin <kmathew@peloton-technologies.com>
2023-07-08 15:09:48 -0700

    frontend_changes

--- frontend/src/Components/Event/Event.js
+++ frontend/src/Components/Event/Event.js
@@ -1,19 +1,19 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-function Event() {
+const Event = (props) => {
     return (
         <Card >
             <Card.Img style={{ height: 250 }} variant="top" src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80/100px180" />
-            <Card.Header>Uvic Programming Club</Card.Header>
+            <Card.Header>{props.name}</Card.Header>
             <Card.Body>
-                <Card.Title>Competitive Programming Contest</Card.Title>
+                <Card.Title>{props.event}</Card.Title>
                 <Card.Text>
-                    With supporting text below as a natural lead-in to additional content.
+                    {props.description}
                 </Card.Text>
                 <Button variant="primary">More info</Button>
             </Card.Body>
-            <Card.Footer className="text-muted text-center">2 days ago</Card.Footer>
+            <Card.Footer className="text-muted text-center">{props.date}</Card.Footer>
         </Card>
     );
 }
